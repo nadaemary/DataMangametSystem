442515ae3df42ab750becc862c0559200491d534
Nada Emari <97479779+nadaemary@users.noreply.github.com>
2024-01-08 15:27:41 +0200

    Last update!
    All files are added except GUI.
    The System is working correctly.

--- Menu.java
+++ Menu.java
@@ -1,14 +1,14 @@
+import java.util.ArrayList;
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Menu {
     private DataManager dataManager;
+    private ArrayList<User> allUsers = new ArrayList<User>();
 
 
-    public Menu(DataManager manager){
+    public Menu(DataManager manager) {
         this.dataManager = manager;
-        System.out.println("Choose which operation you want to perform as follows ");
-        System.out.println("1: for adding a user");
-        System.out.println("2: for deleting a user");
-        System.out.println("3: for updating a user");
-        System.out.println("4: for displaying all users");
     }
 
 
@@ -20,21 +20,111 @@ public class Menu {
         this.dataManager = dataManager;
     }
 
-    public void addInterface(User user1){
+    public ArrayList<User> getAllUsers() {
+        return allUsers;
+    }
+
+    public void setAllUsers(ArrayList<User> allUsers) {
+        this.allUsers = allUsers;
+    }
+
+    public void addInterface(User user1) {
         dataManager.addUser(user1);
     }
 
-    public void deleteInterface(User user1){
+    public void deleteInterface(User user1) {
         dataManager.deleteUser(user1);
     }
 
-    public void displayInterface(){
+    public void displayInterface() {
         dataManager.displayUsers();
     }
 
-    public void updateInterface(User user1, User user2){
+    public void updateInterface(User user1, User user2) {
         dataManager.updateUser(user1, user2);
     }
 
+    public void updateInterface(User user, String name) {
+        dataManager.updateUser(user, name);
+    }
 
+    public void addUerToDatabase(User user) {
+        allUsers.add(user);
+    }
+
+    public void whichUpdateInterface(){
+        System.out.println("choose which update you want to execute");
+        System.out.println("U: update user by user");
+        System.out.println("N: update user's name");
+        Scanner scannerObj = new Scanner(System.in);
+        String updateType = scannerObj.nextLine();
+        switch (updateType){
+            case "U":
+                System.out.println("choose which user you want to be updated");
+                int userToUpdate = scannerObj.nextInt();
+                scannerObj.nextLine();
+                System.out.println("choose which user you want to be updated by");
+                int userToUpdateBy = scannerObj.nextInt();
+                scannerObj.nextLine();
+                updateInterface(allUsers.get(userToUpdate), allUsers.get(userToUpdateBy));
+                break;
+            case "N":
+                int userToUpdate2 = scannerObj.nextInt();
+                scannerObj.nextLine();
+                System.out.println("what is the new name? ");
+                String userNewName = scannerObj.nextLine();
+                updateInterface(allUsers.get(userToUpdate2), userNewName);
+                break;
+        }
+    }
+
+    public void manageInterface() {
+        System.out.println("print outside the do loop");
+        System.out.println("Choose which operation you want to perform as follows ");
+        System.out.println("1: for adding a user");
+        System.out.println("2: for deleting a user");
+        System.out.println("3: for updating a user");
+        System.out.println("4: for displaying all users");
+        System.out.println("5: to exit! ");
+        Scanner scannerObj = new Scanner(System.in);
+        String operaton = scannerObj.nextLine();
+        while (!Objects.equals(operaton, "5")) {
+            switch (operaton) {
+                case "1":
+                    System.out.println("You can add a user");
+                    System.out.println("choose which user you want to add");
+                    int userToAdd = scannerObj.nextInt();
+                    scannerObj.nextLine();
+                    addInterface(allUsers.get(userToAdd));
+                    break;
+                case "2":
+                    System.out.println("You can delete a user");
+                    System.out.println("choose which user you want to delete");
+                    int userToDelete = scannerObj.nextInt();
+                    scannerObj.nextLine();
+                    deleteInterface(allUsers.get(userToDelete));
+                    break;
+                case "3":
+                    System.out.println("You can update a user");
+                    whichUpdateInterface();
+                    break;
+                case "4":
+                    System.out.println("You can display all uers");
+                    displayInterface();
+                    break;
+            }
+            System.out.println("Choose which operation you want to perform as follows ");
+            System.out.println("1: for adding a user");
+            System.out.println("2: for deleting a user");
+            System.out.println("3: for updating a user");
+            System.out.println("4: for displaying all users");
+            System.out.println("5: to exit! ");
+            operaton = scannerObj.nextLine();
+        }
+
+        System.out.println("You finished managing");
+    }
 }
+
+
+
--- Test.java
+++ Test.java
@@ -1,23 +1,37 @@
+import java.util.ArrayList;
 public class Test {
 
 
     public static void main(String[] args) {
+        //ArrayList<User> allUsers = new ArrayList<User>();
+
         System.out.println("This program tests if the CRUD operaton is done correctly");
         DataManager manager1 = new DataManager();
         Menu m1 = new Menu(manager1);
-        Doctor d0 = new Doctor(0, "Harry", "cardiology");
-        Doctor d1 = new Doctor(1, "Ron", "managemetn");
-        Patient p2 = new Patient(2, "Luna", "diabitic");
-        Patient p4 = new Patient(4, "Fred", "cancer");
-        Patient p3 = new Patient(3, "Goorge", "dimensia");
 
-        m1.addInterface(d0);
+        Doctor drHarry = new Doctor(0, "Harry", "cardiology");
+        m1.addUerToDatabase(drHarry);
+        Doctor drRon = new Doctor(1, "Ron", "managemetn");
+        m1.addUerToDatabase(drRon);
+        Patient patientLuna = new Patient(2, "Luna", "diabitic");
+        m1.addUerToDatabase(patientLuna);
+        Patient patientFred = new Patient(4, "Fred", "cancer");
+        m1.addUerToDatabase(patientFred);
+        Patient patientGoorge = new Patient(3, "Goorge", "dimensia");
+        m1.addUerToDatabase(patientGoorge);
+        m1.manageInterface();
         //m1.displayInterface();
-        m1.addInterface(p2);
-        m1.updateInterface(p2,p3);
+
+
+
+        /*
+        m1.addInterface(drHarry);
+        //m1.displayInterface();
+        m1.addInterface(patientLuna);
+        m1.updateInterface(patientLuna,patientFred);
         System.out.println(manager1.getIndeces());
         m1.displayInterface();
-
+*/
         /*
         m1.addInterface(d1);
         m1.addInterface(d2);
